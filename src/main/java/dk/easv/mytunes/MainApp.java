1764bd739945709a4e8e5ed3d1caaac5e9a35203
Tamás Major <majortamas313@gmail.com>
2024-11-26 10:15:05 +0100

    push try

--- src/main/java/dk/easv/mytunes/MainApp.java
+++ src/main/java/dk/easv/mytunes/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("mainScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Helloo!");
         stage.setScene(scene);
         stage.show();
     }
