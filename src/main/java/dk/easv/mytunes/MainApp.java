566a0587430c7f8a968c7bd07214fb18fca5efcd
ervinhun <127263111+ervinhun@users.noreply.github.com>
2024-11-26 10:22:10 +0100

    1st

--- src/main/java/dk/easv/mytunes/MainApp.java
+++ src/main/java/dk/easv/mytunes/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("mainScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Helloo!");
+        stage.setTitle("hi!");
         stage.setScene(scene);
         stage.show();
     }
