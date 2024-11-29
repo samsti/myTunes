baf5e28a73b3593ad989661bdafa6e06e16f16a8
Samuel <114352994+samsti@users.noreply.github.com>
2024-11-29 14:26:29 +0100

    Accidently removed some changes, now it should be fixed. Ervin check it please

--- src/main/java/dk/easv/mytunes/ui/MyTunesModel.java
+++ src/main/java/dk/easv/mytunes/ui/MyTunesModel.java
@@ -8,7 +8,6 @@ import dk.easv.mytunes.exceptions.DBException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MyTunesModel {
